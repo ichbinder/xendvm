e0fa6d4c73a93ba8aa9845195fcc69f98494c446
Jakob Warnow <jakob.warnow@gmx.de>
2014-11-30 17:32:39 +0100

    ipfile fix

--- xdvm.py
+++ xdvm.py
@@ -26,8 +26,8 @@ if __name__ == '__main__':
     cli = Cli.Cli()
     cli.paser()
     
-    ipfreefile = "~/ipfree.txt"
-    ipdropfile = "~/ipdrop.txt"
+    ipfreefile = os.path.join("~/ipfree.txt")
+    ipdropfile = os.path.join("~/ipdrop.txt")
     
     if not os.path.isfile(ipfreefile):
         print "ipfree.txt not found!\n"
